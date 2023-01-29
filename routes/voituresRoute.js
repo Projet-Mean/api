b7ed3e1c25c35da41d623a5e60845cebc4c6fe19
Valisoa Ranaivoson <71868802+lemecrans@users.noreply.github.com>
2023-01-29 23:08:56 +0300

    Update voituresRoute.js

--- routes/voituresRoute.js
+++ routes/voituresRoute.js
@@ -4,7 +4,9 @@ const voitureCtrl = require ("../controllers/voituresController")
 
 router.post('/add',voitureCtrl.AjoutVoiture);
 router.get('/car/:id', voitureCtrl.getOneVoiture);
-router.get('/car', voitureCtrl.DeleteAllVoiture);
+router.get('/cars', voitureCtrl.getAllVoiture);
+router.get('/carsAt', voitureCtrl.getAllVoitureAt);
+router.get('/carsMine/:id',  voitureCtrl.getAllVoitureMine);
 router.put('/car/:id', voitureCtrl.UpdateVoiture);
 router.delete('/delete/:id',voitureCtrl.DeleteAllVoiture);
 module.exports = router;
